51986cc5303f31a66dfef84ba43b5a61bc8b6474
fc49445 <fc49445@alunos.fc.ul.pt>
2021-05-23 01:04:36 +0100

    Attempt at replying to mentions

--- createcorpus.py
+++ createcorpus.py
@@ -1,4 +1,5 @@
 import sys
+import io
 import tweepy
 
 import secret
@@ -12,7 +13,7 @@ def main(username):
     alltweets = []
 
     # make initial request for most recent tweets (200 is the maximum allowed count)
-    new_tweets = api.user_timeline(screen_name=username, count=200)
+    new_tweets = api.user_timeline(screen_name=username, count=200, include_rts=False)
     # save most recent tweets
     alltweets.extend(new_tweets)
     # save the id of the oldest tweet less one
@@ -20,22 +21,24 @@ def main(username):
 
     # keep grabbing tweets until there are no tweets left to grab
     while len(new_tweets) > 0:
+        print(oldest)
         # all subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name=username, count=200, max_id=oldest)
+        new_tweets = api.user_timeline(screen_name=username, count=200, max_id=oldest, include_rts=False)
         # save most recent tweets
         alltweets.extend(new_tweets)
         # update the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1
 
-    # transform the tweepy tweets into a 2D array that will populate the csv
-    with open("corpus.txt", "w") as corpus:
+    # write the tweets in a txt file
+    with io.open("corpus.txt", "w", encoding="utf-8") as corpus:
         for tweet in alltweets:
-            corpus.write("\n".join(tweet.txt))
-
+            corpus.write(tweet.text + "\n")
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        print("Pass the username as arg")
-    username = sys.argv[1]
-    main(username)
+        main("catraslave")
+    else:
+        username = sys.argv[1]
+        main(username)
+
--- emmabot.py
+++ emmabot.py
@@ -1,6 +1,9 @@
+import time
+import io
 import tweepy
-import secret
 
+import tweetgen
+import secret
 
 def main():
     # Twitter authentication
@@ -8,25 +11,51 @@ def main():
     auth.set_access_token(secret.TWITTER_ACCESS, secret.TWITTER_ACCESS_SECRET)
     api = tweepy.API(auth)
 
-
     # Open and read corpus
-    #corpus = open("simple-corpus.txt", "r")
-    #word_lines = corpus.readlines()
-    #corpus.close()
+    corpus = io.open("corpus.txt", "r", encoding="utf-8")
+    word_lines = corpus.readlines()
+    corpus.close()
 
-    # Create word map
-    #word_map = create_word_map(word_lines)
-    #tweet_array = gen_message(word_map)
-    #tweet = ' '.join(tweet_array)
+    # Check if the bot has been mentioned
+    def check_mentions(api, since_id):
+        new_since_id = since_id
+        for tweet in tweepy.Cursor(api.mentions_timeline,
+                                   since_id=since_id).items():
+            new_since_id = max(tweet.id, new_since_id)
+            if tweet.in_reply_to_status_id is not None:
+                continue
 
-
+            # Reply with generated tweet
+            reply_tweet = tweetgen.make_tweet(word_lines)
+            api.update_status(
+                status=reply_tweet,
+                in_reply_to_status_id=tweet.id,
+            )
+        return new_since_id
 
     # Try to tweet
-    try:
-        print("Tweet")
-        #api.update_status(tweet)
-    except tweepy.TweepError:
-        print("Could not tweet")
+    def send_tweet(tweet):
+        try:
+            print(tweet)
+            api.update_status(tweet)
+        except tweepy.TweepError:
+            print("Could not tweet")
+
+    since_id = 1
+    cur_time = 0
+    max_time = 3600
+
+    while True:
+        since_id = check_mentions(api, since_id)
+        cur_time = 60 + cur_time
+
+        if cur_time > max_time:
+            cur_time = 0
+            tweet = tweetgen.make_tweet(word_lines)
+            print(tweet)
+            send_tweet(tweet)
+
+        time.sleep(60)
 
 
 if __name__ == '__main__':
